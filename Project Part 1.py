90708486934cf44a1c7fbd02eb0a1753d468dc04
Tedzeng124 <szeng124@umd.edu>
2024-09-13 22:13:59 -0400

    Update Project Part 1.py

--- Project Part 1.py
+++ Project Part 1.py
@@ -1,2 +1,12 @@
-# 1. Loading the Data
-# Load the dataset from https://catalog.data.gov/dataset/electric-vehicle-population-data into a Pandas DataFrame for initial inspection.
+@@ -2,3 +2,10 @@ Teammates: Sirui Zeng, Xiaolong Zhu, Zhaoyang Pan, Kangrui Kang
+Team name: Nine
+Dataset: 
+    “Electric Vehicle Population Data”
+    https://catalog.data.gov/dataset/electric-vehicle-population-data
+
+Steps:
+    1. Loading the Data
+    2. Inspecting the Data
+    3. Handling Missing Values
+    4. Data Type Conversion
+    5. Handle Categorical Data
